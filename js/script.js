493a460588eebb07fdac0ec1c862557a5fae8d2c
MartaKMB <muszkamarta@interia.pl>
2017-12-18 18:13:33 +0100

    Created content on click on overlap

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 /* navigation */
 const gasaNav = document.querySelector('#gasa_nav');
+const frontNav = document.querySelector('#front_nav');
+const contactNav = document.querySelector('#contact_nav');
 
 /* sections */
 const idImg = document.querySelector('#id_img');
 const idGasa = document.querySelector('#id_content_gasa');
+const idFront = document.querySelector('#id_content_front');
+const idContact = document.querySelector('#id_contact');
 
 
 /* events */
+
 gasaNav.addEventListener('click', (e) => {
-   idImg.style.display = 'none';
+    idImg.style.display = 'none';
+    idFront.style.display = 'none';
+    idContact.style.display = 'none';
+    
 //   idGasa.classList.toggle('invisible_first');
    idGasa.style.display = 'block';
+});
+
+frontNav.addEventListener('click', (e) => {
+    idImg.style.display = 'none';
+    idGasa.style.display = 'none';
+    idContact.style.display = 'none';
+    
+   idFront.style.display = 'block';
+});
+
+contactNav.addEventListener('click', (e) => {
+    idImg.style.display = 'none';
+    idGasa.style.display = 'none';
+    idFront.style.display = 'none';
+    
+    idContact.style.display = 'block';
 });
